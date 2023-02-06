67e22dcb1f056d72ad9aefc2024642558b19d2c5
Zaydrian Price <zaydrianp@gmail.com>
2023-02-06 16:17:35 -0500

    Embed IDs

--- testsearch.py
+++ testsearch.py
@@ -2,7 +2,14 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
 #import testgui
 
-client_credentials_manager = SpotifyClientCredentials() #check definition to change API connection
+#Client ID
+ci = "cb6906d5bc1c4f80bbeb8006ec63bf42"
+
+#Client Secret
+cs = "42f643e0fb444dc1845f08cddc234908"
+
+#Client Id and Secret Id have been embedded to the file so that we can hopelly all work with the API
+client_credentials_manager = SpotifyClientCredentials(client_id=ci, client_secret=cs) #check definition to change API connection
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 print("What would you like to search?")
