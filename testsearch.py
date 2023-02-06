acfe475eb2adc8cfc01b62097b36cd38012bbf5d
clayb <wenzelcl@mail.gvsu.edu>
2023-02-06 14:35:53 -0500

    Groundwork for interacting with Spotify

---
+++ testsearch.py
@@ -0,0 +1,12 @@
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+
+client_credentials_manager = SpotifyClientCredentials() #check definition to change API connection
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+print("What would you like to search?")
+search = str(input())
+print("Searching for: " + search )
+results = sp.search(q=search, limit=20)
+for i, t in enumerate(results['tracks']['items']):
+    print(' ', i+1, t['name'])
