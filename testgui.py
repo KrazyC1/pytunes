050aa5518adc99c2d795db5d533be5a0135b14ed
clayb <wenzelcl@mail.gvsu.edu>
2023-02-20 14:27:17 -0500

    Small program working with spotify searches and TKINTER GUI

--- testgui.py
+++ testgui.py
@@ -1,29 +1,41 @@
 from tkinter import *
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+
+#Client ID
+ci = "cb6906d5bc1c4f80bbeb8006ec63bf42"
+
+#Client Secret
+cs = "42f643e0fb444dc1845f08cddc234908"
+
+#Client Id and Secret Id have been embedded to the file so that we can hopelly all work with the API
+client_credentials_manager = SpotifyClientCredentials(client_id=ci, client_secret=cs) #check definition to change API connection
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 root = Tk()
-root.geometry("300x300")
-root.title(" Q&A ")
+root.geometry("450x450")
+root.title(" Spotify Search ")
 
 def Take_input():
+	Output.delete("1.0","end")
 	INPUT = inputtxt.get("1.0", "end-1c")
-	print(INPUT)
-	if(INPUT == "120"):
-		Output.insert(END, 'Correct')
-	else:
-		Output.insert(END, "Wrong answer")
+	print("Searching for: " + INPUT)
+	results = sp.search(q=INPUT, limit=20)
+	for i, t in enumerate(results['tracks']['items']):
+		Output.insert(END, f"{i+1}. {t['name']}\n")
 	
-l = Label(text = "What is 24 * 5 ? ")
-inputtxt = Text(root, height = 10,
-				width = 25,
+l = Label(text = "What would you like to search? ")
+inputtxt = Text(root, height = 2,
+				width = 50,
 				bg = "light yellow")
 
-Output = Text(root, height = 5,
-			width = 25,
+Output = Text(root, height = 20,
+			width = 50,
 			bg = "light cyan")
 
 Display = Button(root, height = 2,
 				width = 20,
-				text ="Show",
+				text ="Search",
 				command = lambda:Take_input())
 
 l.pack()
