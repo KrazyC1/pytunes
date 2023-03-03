708e9f08206716061d02efd9663f20aeefa852d9
Zaydrian Price <zaydrianp@gmail.com>
2023-03-03 12:30:39 -0500

    Adding editing functions to mp3.py

--- mp3.py
+++ mp3.py
@@ -16,3 +16,18 @@ class Mp3:
     def __str__(self):
         return f"Title: {self.title}\nArtist: {self.artist}\nAlbum: {self.album}\nGenre: {self.genre}"
 
+    def set_title(self,new_title):
+        self.audio['title'] = str(new_title)
+        self.audio.save()
+    
+    def set_artist(self,new_artist):
+        self.audio['artist'] = str(new_artist)
+        self.audio.save()
+    
+    def set_album(self,new_album):
+        self.audio['album'] = str(new_album)
+        self.audio.save()
+    
+    def set_genre(self,new_genre):
+        self.audio['genre'] = str(new_genre)
+        self.audio.save()
