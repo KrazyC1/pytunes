3cebcd45267c3cde8cac86b87874de1b2ae1fabb
Zaydrian Price <zaydrianp@gmail.com>
2023-03-14 13:33:11 -0400

    'spotify search working with Fergalicious'

--- spotify.py
+++ spotify.py
@@ -14,7 +14,29 @@ class Spotify:
         self.sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     
     def search(self,mp3):
-        self.music = mp3()
-        artist = self.sp.search(self.music['artist'],limit=1,type='artist')
+        music = mp3
+        search_result = dict(self.sp.search(music.artist,limit=1,type='artist'))
+        artist = search_result['artists']['items'][0]
+        artist_id = artist['id']
+        list_of_albums = self.sp.artist_albums(artist_id)['items']
+        for i in range(len(list_of_albums)):
+            if (list_of_albums[i]['name']==music.album):
+                list_of_tracks = self.sp.album_tracks(album_id=list_of_albums[i]['id'])
+                for j in range(len(list_of_tracks)):
+                    if (list_of_tracks['items'][j]['name']==music.title):
+                        print(artist['name'])
+                        print(artist['genres'][0])
+                        print(list_of_tracks['items'][j]['name'])
+                        print(list_of_albums[i]['name'])
+                        track_id=list_of_tracks['items'][j]['id']
+                        break
+                break 
+                        
+                
         
-    
+home = '/Users/zaydrianprice/Documents'
+spotify_tagger = '/spotify-tagger/music'
+test_file = home + spotify_tagger + '/01 Fergalicious (Feat. Will.I.Am).mp3'
+test_music = mp3.Mp3(test_file)
+test_spotify=Spotify()
+test_spotify.search(test_music)
