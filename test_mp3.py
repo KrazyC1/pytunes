3f812fe4bf2ba6e6b7bc621d4b9fad9b6fc628db
MatteoCiavaglia <matteo3.ciavaglia@gmail.com>
2023-04-12 18:09:55 -0400

    Unit Test Completed For Mp3 And Spotify Classes

--- test_mp3.py
+++ test_mp3.py
@@ -3,10 +3,12 @@ import mp3
 
 class TestMp3(unittest.TestCase):
     
-    SAMPLE_FOLDER = '/Users/zaydrianprice/Documents/GitHub'
+    SAMPLE_FOLDER = 'C:./Users/matte/OneDrive/Documents/IntroToSoftwareEgineering'
     SPOTIFY_TAGGER = '/spotify-tagger/music/'
-    FILE_PATH_1 = str(SAMPLE_FOLDER + SPOTIFY_TAGGER + '01 Fergalicious (Feat. Will.I.Am).mp3')
-    FILE_PATH_2 = str(SAMPLE_FOLDER + SPOTIFY_TAGGER + '01 Forever (Main Version) 1.mp3')
+    #FILE_PATH_1 = str(SAMPLE_FOLDER + SPOTIFY_TAGGER + '01 Fergalicious (Feat. Will.I.Am).mp3')
+    #FILE_PATH_2 = str(SAMPLE_FOLDER + SPOTIFY_TAGGER + '01 Forever (Main Version) 1.mp3')
+    FILE_PATH_1 = 'C:/Users/matte/OneDrive/Documents/IntroToSoftwareEgineering/spotify-tagger/music/01 Fergalicious (Feat. Will.I.Am).mp3'
+    FILE_PATH_2 = 'C:/Users/matte/OneDrive/Documents/IntroToSoftwareEgineering/spotify-tagger/music/01 Forever (Main Version) 1.mp3'
     
     test_music_1 = mp3.Mp3(FILE_PATH_1)
     test_music_2 = mp3.Mp3(FILE_PATH_2)
@@ -23,8 +25,10 @@ class TestMp3(unittest.TestCase):
         self.assertEqual(TestMp3.test_music_1.get_album(), 'The Dutchess')
         self.assertEqual(TestMp3.test_music_2.get_album(), 'Forever')
         
+    def test_genre(self):
+        TestMp3.test_music_1.set_genre("['dance pop', 'pop', 'post-teen pop', 'rap']")
+        TestMp3.test_music_2.set_genre("['dance pop', 'r&b']")
+        self.assertEqual(TestMp3.test_music_1.get_genre(), "['dance pop', 'pop', 'post-teen pop', 'rap']")
+        self.assertEqual(TestMp3.test_music_2.get_genre(), "['dance pop', 'r&b']")
 if __name__ == '__main__':
     unittest.main()
-
-
-
--- test_spotify.py
+++ test_spotify.py
@@ -1,10 +1,43 @@
+import unittest
 import spotify
-import mp3
+from spotify import Spotify
+from mp3 import Mp3
 
-home = '/Users/zaydrianprice/Documents/GitHub'
-spotify_tagger = '/spotify-tagger/music'
-test_file = home + spotify_tagger + '/01 Fergalicious (Feat. Will.I.Am).mp3'
-test_music = mp3.Mp3(test_file)
-test_spotify=spotify.Spotify()
-test_spotify.sync_spotify(test_music)
-print(test_music)
+class TestSpotify(unittest.TestCase):
+    def setUp(self):
+        self.spotify = Spotify()
+        test_music = Mp3('C:/Users/matte/OneDrive/Documents/IntroToSoftwareEgineering/spotify-tagger/music/01 James Bay - Let It Go.mp3')
+        test_music2 = Mp3('C:/Users/matte/OneDrive/Documents/IntroToSoftwareEgineering/spotify-tagger/music/01 Michael Jackson - Billie Jean.mp3')
+        test_spotify=spotify.Spotify()
+        self.test_results = test_spotify.sync_spotify(test_music)
+        self.test_results2 = test_spotify.sync_spotify(test_music2)
+        
+
+    def test_spotify(self):
+        test_music = Mp3('C:/Users/matte/OneDrive/Documents/IntroToSoftwareEgineering/spotify-tagger/music/01 James Bay - Let It Go.mp3')
+        expected_output = {
+    'title': 'Let It Go',
+    'album': 'Chaos And The Calm',
+    'artist': 'James Bay',
+    'id': '3ZQsCvbpp2HVXt9Mp46f8n',
+    'genre': ['neo mellow', 'pop', 'pop rock'],
+    'date': '2015-03-25'
+}
+        actual_output = self.spotify.search(test_music)
+        self.assertEqual(actual_output, expected_output)
+
+    def test_spotify2(self):
+        test_music2 = Mp3('C:/Users/matte/OneDrive/Documents/IntroToSoftwareEgineering/spotify-tagger/music/01 Michael Jackson - Billie Jean.mp3')
+        expected_output = {
+    'title': 'Billie Jean',
+    'album': 'Thriller',
+    'artist': 'Michael Jackson',
+    'id': '7J1uxwnxfQLu4APicE5Rnj',
+    'genre': ['pop', 'r&b', 'soul'],
+    'date': '1982-11-30'
+}
+        actual_output = self.spotify.search(test_music2)
+        self.assertEqual(actual_output, expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
