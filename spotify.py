b7d229dec90342a2ff584fe73dbb5e210c6f0401
Zaydrian Price <zaydrianp@gmail.com>
2023-04-12 22:56:00 -0400

    style updated

--- spotify.py
+++ spotify.py
@@ -1,7 +1,10 @@
+"""This is a module to creates an object for the Spotify API."""
+# pylint: disable=too-many-locals
+# pylint: disable=unused-variable
+# pylint: disable=invalid-name
+
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import mp3
-
 
 
 class Spotify:
@@ -17,11 +20,11 @@ class Spotify:
         # Client Secret
         self.cs = "42f643e0fb444dc1845f08cddc234908"
 
-        # Client Id and Secret Id have been embedded to the file so that we 
+        # Client Id and Secret Id have been embedded to the file so that we
         # can hopelly all work with the API
         client_credentials_manager = SpotifyClientCredentials(
-            client_id=self.ci, client_secret=self.cs)  
-            # check definition to change API connection
+            client_id=self.ci, client_secret=self.cs)
+        # check definition to change API connection
         self.sp = spotipy.Spotify(
             client_credentials_manager=client_credentials_manager)
 
@@ -42,7 +45,7 @@ class Spotify:
         print("Artist to be searched is: " + search_artist)
         search_album = str(mp3.get_album())
         print("Album to be searched is: " + search_album + "\n")
-        
+
         search_result = self.sp.search(q=search_artist, type='artist', limit=1)
         print(search_result)
         artist_name = search_result['artists']['items'][0]['name']
@@ -51,7 +54,7 @@ class Spotify:
         song_album = None
         song_album_id = None
         song_album_date = None
-        
+
         artist_albums = self.sp.artist_albums(artist_id=artist_id)['items']
         for i, album in enumerate(artist_albums):
             if album['name'] == search_album:
@@ -61,10 +64,13 @@ class Spotify:
                 break
 
         if song_album is None or song_album_id is None:
-            print(f"Album '{search_album}' not found for artist '{search_artist}'. Searching for song without album...")
-            fallback_search = self.sp.search(q=f'artist:{search_artist} track:{search_title}', type='track', limit=1)
+            print(f"Album '{search_album}' not found for artist '\
+                {search_artist}'. Searching for song without album...")
+            fallback_search = self.sp.search(q=f'artist:{search_artist}\
+                track:{search_title}', type='track', limit=1)
             if not fallback_search['tracks']['items']:
-                raise ValueError(f"Song '{search_title}' not found for artist '{search_artist}'")
+                raise ValueError(f"Song '{search_title}' not found for \
+                    artist '{search_artist}'")
             track = fallback_search['tracks']['items'][0]
             song_title = track['name']
             song_id = track['id']
@@ -72,25 +78,27 @@ class Spotify:
             song_album_id = track['album']['id']
             song_album_date = track['album']['release_date']
         else:
-            album_songs = self.sp.album_tracks(album_id=song_album_id)['items']
+            album_songs = self.sp.album_tracks(album_id=song_album_id)['items\
+                ']
             for j, song in enumerate(album_songs):
                 if song['name'] == search_title:
                     song_title = song['name']
                     song_id = song['id']
-                    break  
-        
+                    break
+
         album_songs = self.sp.album_tracks(album_id=song_album_id)['items']
         for j, song in enumerate(album_songs):
             if song['name'] == search_title:
                 song_title = song['name']
                 song_id = song['id']
-                break  
-        
-        results = {'title': song_title, 'album': song_album, 'artist': artist_name, 'id': song_id, 'genre': artist_genre, 'date': song_album_date}
+                break
+
+        results = {'title': song_title, 'album': song_album,
+                   'artist': artist_name, 'id': song_id,
+                   'genre': artist_genre, 'date': song_album_date}
         print(results)
         return results
-        
-        
+
     def sync_spotify(self, mp3):
         """
             This function uses the search() method to take an mp3, search for
