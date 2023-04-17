db61dac78dfdcfca2b628e262d6dbd9e708c5c5d
clayb <wenzelcl@mail.gvsu.edu>
2023-04-17 15:28:30 -0400

    relative path for test_spotify.py

--- test_spotify.py
+++ test_spotify.py
@@ -6,15 +6,15 @@ from mp3 import Mp3
 class TestSpotify(unittest.TestCase):
     def setUp(self):
         self.spotify = Spotify()
-        test_music = Mp3('C:/Users/matte/OneDrive/Documents/IntroToSoftwareEgineering/spotify-tagger/music/01 James Bay - Let It Go.mp3')
-        test_music2 = Mp3('C:/Users/matte/OneDrive/Documents/IntroToSoftwareEgineering/spotify-tagger/music/01 Michael Jackson - Billie Jean.mp3')
+        test_music = Mp3('music/01 James Bay - Let It Go.mp3')
+        test_music2 = Mp3('music/01 Michael Jackson - Billie Jean.mp3')
         test_spotify=spotify.Spotify()
         self.test_results = test_spotify.sync_spotify(test_music)
         self.test_results2 = test_spotify.sync_spotify(test_music2)
         
 
     def test_spotify(self):
-        test_music = Mp3('C:/Users/matte/OneDrive/Documents/IntroToSoftwareEgineering/spotify-tagger/music/01 James Bay - Let It Go.mp3')
+        test_music = Mp3('music/01 James Bay - Let It Go.mp3')
         expected_output = {
     'title': 'Let It Go',
     'album': 'Chaos And The Calm',
@@ -27,7 +27,7 @@ class TestSpotify(unittest.TestCase):
         self.assertEqual(actual_output, expected_output)
 
     def test_spotify2(self):
-        test_music2 = Mp3('C:/Users/matte/OneDrive/Documents/IntroToSoftwareEgineering/spotify-tagger/music/01 Michael Jackson - Billie Jean.mp3')
+        test_music2 = Mp3('music/01 Michael Jackson - Billie Jean.mp3')
         expected_output = {
     'title': 'Billie Jean',
     'album': 'Thriller',
