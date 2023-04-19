65e0d79cb4952f2abc954ca947065f81de52ab38
clayb <wenzelcl@mail.gvsu.edu>
2023-04-19 11:17:35 -0400

    added onto test file slightly

--- test_mp3.py
+++ test_mp3.py
@@ -1,35 +1,37 @@
 import unittest
 import mp3
 
-class TestMp3(unittest.TestCase):
+class TestSpotifyTagger(unittest.TestCase):
     
-    #SAMPLE_FOLDER = 'C:./Users/matte/OneDrive/Documents/IntroToSoftwareEgineering'
-    SPOTIFY_TAGGER = '/spotify-tagger/music/'
-    #FILE_PATH_1 = str(SAMPLE_FOLDER + SPOTIFY_TAGGER + '01 Fergalicious (Feat. Will.I.Am).mp3')
-    #FILE_PATH_2 = str(SAMPLE_FOLDER + SPOTIFY_TAGGER + '01 Forever (Main Version) 1.mp3')
     FILE_PATH_1 = 'music/01 Fergalicious (Feat. Will.I.Am).mp3'
     FILE_PATH_2 = 'music/01 Forever (Main Version) 1.mp3'
     
     test_music_1 = mp3.Mp3(FILE_PATH_1)
     test_music_2 = mp3.Mp3(FILE_PATH_2)
-    
-    def test_title(self):
-        self.assertEqual(TestMp3.test_music_1.get_title(), 'Fergalicious')
-        self.assertEqual(TestMp3.test_music_2.get_title(), 'Forever')
-        
-    def test_artist(self):
-        self.assertEqual(TestMp3.test_music_1.get_artist(), 'Fergie')
-        self.assertEqual(TestMp3.test_music_2.get_artist(), 'Chris Brown')
-        
-    def test_album(self):
-        self.assertEqual(TestMp3.test_music_1.get_album(), 'The Dutchess')
-        self.assertEqual(TestMp3.test_music_2.get_album(), 'Forever')
-        
-    def test_genre(self):
-        TestMp3.test_music_1.set_genre("['dance pop', 'pop', 'post-teen pop', 'rap']")
-        TestMp3.test_music_2.set_genre("['dance pop', 'r&b']")
-        self.assertEqual(TestMp3.test_music_1.get_genre(), "['dance pop', 'pop', 'post-teen pop', 'rap']")
-        self.assertEqual(TestMp3.test_music_2.get_genre(), "['dance pop', 'r&b']")
+
+    def test_file_path(self):
+        self.assertEqual(self.test_music_1.file_path, self.FILE_PATH_1)
+        self.assertEqual(self.test_music_2.file_path, self.FILE_PATH_2)
+
+    def test_length(self):
+        self.assertIsInstance(self.test_music_1.length, int)
+        self.assertIsInstance(self.test_music_2.length, int)
+
+    def test_date(self):
+        self.test_music_1.set_date("2006-09-13")
+        self.test_music_2.set_date("2008-06-09")
+        self.assertEqual(self.test_music_1.get_date()[:4], "2006")
+        self.assertEqual(self.test_music_2.get_date()[:4], "2008")
+
+    def test_modify_metadata(self):
+        self.test_music_1.set_title("New Title")
+        self.test_music_1.set_artist("New Artist")
+        self.test_music_1.set_album("New Album")
+        self.test_music_1.set_genre("New Genre")
+        self.assertEqual(self.test_music_1.get_title(), "New Title")
+        self.assertEqual(self.test_music_1.get_artist(), "New Artist")
+        self.assertEqual(self.test_music_1.get_album(), "New Album")
+        self.assertEqual(self.test_music_1.get_genre(), "New Genre")
 
     def test_import_multiple_mp3_files(self):
         folder_path = 'music/'
@@ -40,6 +42,5 @@ class TestMp3(unittest.TestCase):
             mp3_list.append(mp3.Mp3(file_path))
         self.assertEqual(len(mp3_list), 2)
 
-        
 if __name__ == '__main__':
     unittest.main()
